a5490d3e28a9a9b597d6c430b2d785ec733fc02d
JibHop <enda29003@gmail.com>
2024-06-18 22:56:42 +0100

    rename Examplemod to TorasEquusDecorMod

--- src/main/java/com/equus/torasequusdecormod/TorasEquusDecorMod.java
+++ src/main/java/com/equus/torasequusdecormod/TorasEquusDecorMod.java
@@ -18,14 +18,14 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MOD_ID)
-public class ExampleMod
+@Mod(TorasEquusDecorMod.MOD_ID)
+public class TorasEquusDecorMod
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     public static final String MOD_ID = "tedm";
 
-    public ExampleMod()
+    public TorasEquusDecorMod()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
