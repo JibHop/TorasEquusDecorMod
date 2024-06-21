b697f43ec52cc59d65c48669d1e02d07ce317992
JibHop <enda29003@gmail.com>
2024-06-21 19:01:10 +0100

    Add Items Brush and CurryComb

--- src/main/java/com/equus/torasequusdecormod/TorasEquusDecorMod.java
+++ src/main/java/com/equus/torasequusdecormod/TorasEquusDecorMod.java
@@ -1,10 +1,12 @@
 package com.equus.torasequusdecormod;
 
+import com.equus.torasequusdecormod.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -27,8 +29,10 @@ public class TorasEquusDecorMod
 
     public TorasEquusDecorMod()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
+        ModItems.register(eventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/com/equus/torasequusdecormod/item/ModItems.java
@@ -0,0 +1,24 @@
+package com.equus.torasequusdecormod.item;
+
+import com.equus.torasequusdecormod.TorasEquusDecorMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TorasEquusDecorMod.MOD_ID);
+
+    public static final RegistryObject<Item> Brush = ITEMS.register("brush",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+    public static final RegistryObject<Item> CurryComb = ITEMS.register("currycomb",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
